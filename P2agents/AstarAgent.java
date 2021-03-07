7134f08090da6f15e184e420e7123d62a17aa69b
Alexander Harding Bradley <Alexanderhb02@gmail.com>
2021-03-06 19:24:03 -0500

    ach159 fixed bug where best move was staying in place

--- P2agents/AstarAgent.java
+++ P2agents/AstarAgent.java
@@ -381,6 +381,7 @@ public class AstarAgent extends Agent {
 
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) continue; // Ignore the step we are already at
 
                 int tx = currLoc.x + i;
                 int ty = currLoc.y + j;
